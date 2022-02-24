b3947b6796b6b456912c099e241fc1def984be89
Lucas Rodrigues Fiori <lucas.fiori@corebiz.ag>
2022-02-23 22:23:56 -0300

    push

--- App.py
+++ App.py
@@ -1,8 +1,14 @@
 arq = open("./src/txt.txt", mode="r", encoding="utf-8")
+tokens=["if","showthis","else", "=", "==", "*", "/", "%", "!=", "int", "float", "string", "boolean", "else if", "{", "}",
+"(",")", "[","]", "for", "while", ";"]
+
 linhas = arq.readlines()
 i = 0
 for linha in linhas:
-
     i+=1
+    for token in linha:
+        if token in tokens:
+            print(token)
+
     print("linha {} {}".format(i, linha))
     print("")
